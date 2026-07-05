f988dd710716fba0090b7a4ad7d959fd45730625
sock <r.p.skiles@gmail.com>
2026-07-05 04:20:52 -0400

    Use OSC52 clipboard over SSH

--- internal/tui/clipboard_other.go
+++ internal/tui/clipboard_other.go
@@ -2,8 +2,29 @@
 
 package tui
 
-import "github.com/atotto/clipboard"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/atotto/clipboard"
+	"github.com/aymanbagabas/go-osc52/v2"
+)
 
 func writeClipboard(text string) error {
+	if runningOverSSH() {
+		seq := osc52.New(text)
+		if os.Getenv("TMUX") != "" {
+			seq = seq.Tmux()
+		}
+		_, err := fmt.Fprint(os.Stderr, seq.String())
+		return err
+	}
 	return clipboard.WriteAll(text)
+}
+
+func runningOverSSH() bool {
+	return os.Getenv("SSH_CONNECTION") != "" ||
+		os.Getenv("SSH_CLIENT") != "" ||
+		strings.TrimSpace(os.Getenv("SSH_TTY")) != ""
 }
