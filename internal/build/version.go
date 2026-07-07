34e7468d07b3fa5287ea8fcffb6f16ab819057a6
sock <r.p.skiles@gmail.com>
2026-07-07 19:47:46 -0400

    Bump version to v0.1.23

--- internal/build/version.go
+++ internal/build/version.go
@@ -4,4 +4,4 @@ package build
 // Version is the release tag. Set at build time with:
 //
 //	go build -ldflags "-X github.com/sockheadrps/llmctl/internal/build.Version=v0.2.0" .
-var Version = "v0.1.22"
+var Version = "v0.1.23"
